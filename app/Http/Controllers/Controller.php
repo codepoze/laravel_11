3bb9b17a5d31b4c7a0b4059f12fe0ee7f093191d
Alan Lengkoan <alansaputralengkoan@gmail.com>
2025-05-27 18:21:11 +0800

    update akun

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
-    //
+    // deklarasi variabel global session
+    public $session;
+
+    public function __construct()
+    {
+        // untuk session user
+        $this->session = Auth::user();
+    }
 }
